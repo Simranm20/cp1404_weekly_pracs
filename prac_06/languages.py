6f926d5952cb24cdb703b9c27ae551b66216fcfd
jc957983 <simrankishin.mulchandani@my.jcu.edu.au>
2022-10-25 19:27:40 +1000

    languages.py completed as required

---
+++ prac_06/languages.py
@@ -0,0 +1,12 @@
+"""
+start time: 6:20 pm
+End time: 6:45 pm
+"""
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
