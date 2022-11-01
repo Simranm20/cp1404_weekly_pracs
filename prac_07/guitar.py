e08b2856e0b233eb772aeef3ef001f83cc3138f1
jc957983 <simrankishin.mulchandani@my.jcu.edu.au>
2022-11-01 17:24:19 +1000

    start guitar.py

---
+++ prac_07/guitar.py
@@ -0,0 +1,13 @@
+class Guitar:
+    """Represent information about a guitar."""
+
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+    # copy the idea from programming language
+
+    def __str__(self):
+        """Return string representation of a guitar."""
+        return f"{self.name}, Made in {self.year}, ${self.cost}"
+
