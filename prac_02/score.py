8e505ccf831466343786a69e1e370d3002a90b1c
GUStudent <simrankishin.mulchandani@my.jcu.edu.au>
2022-11-02 04:42:10 +1000

    score.py updates as required

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,19 +1,28 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404/CP5632 - Practical 2
+Fixed program to determine score status, with function
 """
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(results(score))
+    print("Random score", results(random.randint(0, 100)))
+
+
+def results(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
     elif score >= 90:
-        print("Excellent")
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
+
+
+main()
